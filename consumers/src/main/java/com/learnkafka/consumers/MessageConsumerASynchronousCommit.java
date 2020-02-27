b59ddb2e70990b34fbdf4a5f8ed30fa620e6fd5f
dilipsundarraj <dilip.bandlasundarraj@gmail.com>
2020-02-27 15:55:39 -0600

    managing offsets

---
+++ consumers/src/main/java/com/learnkafka/consumers/MessageConsumerASynchronousCommit.java
@@ -0,0 +1,72 @@
+package com.learnkafka.consumers;
+
+import org.apache.kafka.clients.consumer.CommitFailedException;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MessageConsumerASynchronousCommit {
+
+    private static final Logger logger = LoggerFactory.getLogger(MessageConsumerASynchronousCommit.class);
+    KafkaConsumer<String, String> kafkaConsumer;
+    String topicName = "test-topic-replicated";
+
+    public MessageConsumerASynchronousCommit(Map<String, Object> propsMap) {
+        kafkaConsumer = new KafkaConsumer<String, String>(propsMap);
+    }
+
+    public static Map<String, Object> buildConsumerProperties() {
+
+        Map<String, Object> propsMap = new HashMap<>();
+
+        propsMap.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092,localhost:9093,localhost:9094");
+        propsMap.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        propsMap.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        propsMap.put(ConsumerConfig.GROUP_ID_CONFIG, "messageconsumer");
+        propsMap.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
+        return propsMap;
+    }
+
+    public void pollKafka() {
+        kafkaConsumer.subscribe(List.of(topicName));
+        Duration timeOutDuration = Duration.of(100, ChronoUnit.MILLIS);
+        try {
+            while (true) {
+                ConsumerRecords<String, String> consumerRecords = kafkaConsumer.poll(timeOutDuration);
+                consumerRecords.forEach((record) -> {
+                    logger.info("Consumer Record Key is {} and the value is {} and the partion {}",
+                            record.key(), record.value(), record.partition());
+                });
+                //kafkaConsumer.commitAsync();
+                kafkaConsumer.commitAsync((offsets, exception) -> {
+                    if(exception!=null){
+                        logger.error("Exception Committing the offsets : {} " , exception.getMessage());
+                    }else{
+                        logger.info("Offsets are committed successfully");
+                    }
+                }); // commits the last record offset read by the poll method
+            }
+        } catch (CommitFailedException e) {
+            logger.error("CommitFailedException in pollKafka : " + e);
+        } catch (Exception e) {
+            logger.error("Exception in pollKafka : " + e);
+        } finally {
+            kafkaConsumer.close();
+        }
+
+    }
+
+    public static void main(String[] args) {
+        MessageConsumerASynchronousCommit messageConsumer = new MessageConsumerASynchronousCommit(buildConsumerProperties());
+        messageConsumer.pollKafka();
+    }
+}
